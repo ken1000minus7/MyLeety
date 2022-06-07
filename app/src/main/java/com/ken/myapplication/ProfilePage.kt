44bb962cd1baf242646d5114bb26ae0972d92cf8
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-06-08 03:26:24 +0530

    i m trying ok

--- app/src/main/java/com/ken/myapplication/ProfilePage.kt
+++ app/src/main/java/com/ken/myapplication/ProfilePage.kt
@@ -1,16 +1,18 @@
 package com.ken.myapplication
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Text
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -18,23 +20,57 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfilePage(){
-    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Image of ma boi",
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Card(
             modifier = Modifier
                 .size(150.dp)
-                .clip(CircleShape),
-            contentScale = ContentScale.Crop
-        )
+                .clip(CircleShape)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "Image of ma boi",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+        }
+
         Text(
             text = "Leety lover",
             fontSize = 25.sp,
             fontWeight = FontWeight.Bold
         )
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            QuestionStats(title = "Easy", value = 20, color = Color.Green)
+            QuestionStats(title = "Medium", value = 90, color = Color(0xFFC94E0C))
+            QuestionStats(title = "Hard", value = 11, color = Color.Red)
+        }
         Text(text = "I love leety more than anything else, please leety give me lyf")
+        Button(onClick = {}) {
+            Text(text = "Clicky ma boi")
+        }
+    }
+}
+
+@Composable
+fun QuestionStats(title : String, value : Int, color : Color){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(10.dp)
+    ) {
+        Text(text = value.toString(), fontWeight = FontWeight.Bold, fontSize = 20.sp)
+        Text(text = title, fontSize = 18.sp, color = color)
     }
 }
 
