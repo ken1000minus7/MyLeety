d6d6ad8a4353d0f4c1199bd85f374231e5792ca9
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-06-08 19:42:30 +0530

    I keep on moving forward (｡◕‿◕｡)

--- app/src/main/java/com/ken/myapplication/MainPage.kt
+++ app/src/main/java/com/ken/myapplication/MainPage.kt
@@ -1,19 +1,52 @@
 package com.ken.myapplication
 
-import androidx.compose.foundation.layout.Column
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainPage() {
-    Column {
-        Text(text = "Lol")
+    var nameState by rememberSaveable {
+        mutableStateOf("")
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Welcome", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            value = nameState,
+            onValueChange = {
+                nameState = it
+            },
+            placeholder = {
+                Text(text = "elo")
+            }
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        ElevatedButton(onClick = {
+            Log.d("kaka",nameState)
+        }) {
+            Text(text = "Submit")
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun MainPagePreview(){
+private fun MainPagePreview(){
     MainPage()
 }
--- app/src/main/java/com/ken/myapplication/ProfilePage.kt
+++ app/src/main/java/com/ken/myapplication/ProfilePage.kt
@@ -16,49 +16,150 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.compose.layoutId
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfilePage(){
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(20.dp)
-            .verticalScroll(rememberScrollState())
-    ) {
-        Card(
+    BoxWithConstraints {
+        val constraintSet = if(minWidth < 600.dp) portraitConstraints() else landscapeConstraints()
+
+        ConstraintLayout(
+            constraintSet,
             modifier = Modifier
-                .size(150.dp)
-                .clip(CircleShape)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            animateChanges = true
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = "Image of ma boi",
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
+            ElevatedCard(
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(CircleShape)
+                    .layoutId("image")
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Image of ma boi",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+            }
+
+            Text(
+                text = "Leety lover",
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.layoutId("name")
             )
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .layoutId("quesStats")
+            ) {
+                QuestionStats(title = "Easy", value = 20, color = Color.Green)
+                QuestionStats(title = "Medium", value = 90, color = Color(0xFFC94E0C))
+                QuestionStats(title = "Hard", value = 11, color = Color.Red)
+            }
+            Text(
+                text = "I love leety more than anything else, please leety give me lyf",
+                modifier = Modifier.layoutId("about")
+            )
+            Button(
+                onClick = {},
+                modifier = Modifier.layoutId("button")
+            ) {
+                Text(text = "Clicky ma boi")
+            }
+        }
+    }
+}
+
+private fun portraitConstraints() : ConstraintSet{
+    return ConstraintSet {
+        val image = createRefFor("image")
+        val name = createRefFor("name")
+        val quesStats = createRefFor("quesStats")
+        val about = createRefFor("about")
+        val button = createRefFor("button")
+
+        constrain(image){
+            top.linkTo(parent.top, margin = 20.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
         }
 
-        Text(
-            text = "Leety lover",
-            fontSize = 25.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            QuestionStats(title = "Easy", value = 20, color = Color.Green)
-            QuestionStats(title = "Medium", value = 90, color = Color(0xFFC94E0C))
-            QuestionStats(title = "Hard", value = 11, color = Color.Red)
+        constrain(name){
+            top.linkTo(image.bottom, margin = 10.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
         }
-        Text(text = "I love leety more than anything else, please leety give me lyf")
-        Button(onClick = {}) {
-            Text(text = "Clicky ma boi")
+
+        constrain(quesStats) {
+            top.linkTo(name.bottom)
+        }
+
+        constrain(about) {
+            top.linkTo(quesStats.bottom)
+            start.linkTo(parent.start, margin = 20.dp)
+            end.linkTo(parent.end, margin = 20.dp)
+            width = Dimension.fillToConstraints
+        }
+
+        constrain(button){
+            top.linkTo(about.bottom, margin = 15.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+    }
+}
+
+private fun landscapeConstraints() : ConstraintSet{
+    return ConstraintSet {
+        val image = createRefFor("image")
+        val name = createRefFor("name")
+        val quesStats = createRefFor("quesStats")
+        val about = createRefFor("about")
+        val button = createRefFor("button")
+
+        constrain(image){
+            top.linkTo(parent.top, margin = 20.dp)
+            start.linkTo(parent.start, margin = 20.dp)
+        }
+
+        constrain(name){
+            top.linkTo(image.top)
+            start.linkTo(image.end, margin = 20.dp)
+            end.linkTo(parent.end, margin = 20.dp)
+        }
+
+        constrain(quesStats) {
+            top.linkTo(about.bottom)
+            start.linkTo(image.end)
+            end.linkTo(parent.end)
+            width = Dimension.fillToConstraints
+        }
+
+        constrain(about) {
+            top.linkTo(name.bottom)
+            start.linkTo(image.end, margin = 20.dp)
+            end.linkTo(parent.end, margin = 20.dp)
+            width = Dimension.wrapContent
+        }
+
+        constrain(button){
+            top.linkTo(quesStats.bottom, margin = 15.dp)
+            start.linkTo(image.end)
+            end.linkTo(parent.end)
         }
     }
 }
@@ -76,6 +177,16 @@ fun QuestionStats(title : String, value : Int, color : Color){
 
 @Preview(showBackground = true)
 @Composable
-fun ProfilePagePreview(){
+private fun ProfilePagePreview(){
+    ProfilePage()
+}
+
+@Preview(
+    showBackground = true,
+    device = Devices.AUTOMOTIVE_1024p,
+    widthDp = 1024
+)
+@Composable
+private fun ProfilePageLandscapePreview(){
     ProfilePage()
 }
